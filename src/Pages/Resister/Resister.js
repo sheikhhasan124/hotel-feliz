768bf2c30f2ae8410ccacd649f76029ff6e2464d
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-12 13:55:44 +0600

    resister page create done

--- src/Pages/Resister/Resister.js
+++ src/Pages/Resister/Resister.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Resister.css'
+import Logo from '../../imges/google.svg'
 
 const Resister = () => {
     return (
-        <div>
-            <h2>resister</h2>
+        <div className='form-container'>
+            <div className="form">
+               <h2 className="title">Sign Up</h2>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="text" name="password" id="" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input type="text" name="confirm-password" id="" />
+                    </div>
+                    <div className="signup-btn">
+                        <button>Sign Up</button>
+                    </div>
+                </form>
+                <div>
+                    <p>Already have an accoutn? <Link className='form-login' to="/login">Login</Link></p>
+                </div>
+                <div className='form-horizontal'>
+                    <div className="hr"></div>
+                    <p>or</p>
+                    <div className="hr"></div>
+                </div>
+                <div>
+                    <button className='google-btn'> <img src={Logo} alt="" /> <p>Continue With Google</p></button>
+                </div>
+            </div>
         </div>
     );
 };
